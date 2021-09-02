bb4f652e345363ec66aa09ec631662a9f770bc8c
ViniciusMossi <vini_agg@hotmail.com>
2021-09-02 11:18:14 -0400

    verifica req 1

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,17 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { LOGIN } from '../actions';
+
+const INITIAL_STATE = {
+  email: '',
+};
+
+export default function user(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case LOGIN:
+    return {
+      ...state,
+      email: action.payload.email };
+  default:
+    return state;
+  }
+}

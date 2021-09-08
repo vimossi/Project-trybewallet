e744d464637a1339f3bbdd022d52c0d1353cc22a
ViniciusMossi <vini_agg@hotmail.com>
2021-09-08 11:22:28 -0400

    implementa header e formulario wallet

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,16 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+// teste
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+export default function wallet(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case 'CURRENCIES':
+    return { ...state, currencies: action.currencies };
+  case 'EXPENSES':
+    return { ...state, expenses: action.expenses };
+  default:
+    return state;
+  }
+}
